dc058d9f37186c661266d2612cc66e0cbf6378a5
bheki97 <bfmokoena7@gmail.com>
2023-11-30 14:50:05 +0200

    Initial commit

---
+++ src/main/java/com/fnb/bheki97/chatappbackendspringboot/ChatappBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fnb.bheki97.chatappbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fnb/bheki97/chatappbackendspringboot/ChatappBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnb.bheki97.chatappbackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
