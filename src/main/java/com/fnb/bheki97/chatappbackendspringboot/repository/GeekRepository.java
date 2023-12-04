94c3dcb8fde6d93ed803048748c4750d99f473bc
bheki97 <bfmokoena7@gmail.com>
2023-12-04 12:22:00 +0200

    updated geek entity, and added a Jpa repo

--- src/main/java/com/fnb/bheki97/chatappbackendspringboot/repository/GeekRepository.java
+++ src/main/java/com/fnb/bheki97/chatappbackendspringboot/repository/GeekRepository.java
@@ -1,8 +1,9 @@
 package com.fnb.bheki97.chatappbackendspringboot.repository;
 
 import com.fnb.bheki97.chatappbackendspringboot.entity.Geek;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GeekRepository  {
-//extends JpaRepository<Geek,String>
+public interface GeekRepository extends JpaRepository<Geek,String> {
+
 
 }
