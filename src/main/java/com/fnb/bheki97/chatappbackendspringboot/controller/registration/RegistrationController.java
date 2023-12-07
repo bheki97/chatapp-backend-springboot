2844dc14d088070495883951d954b602918f3003
bheki97 <bfmokoena7@gmail.com>
2023-12-07 16:53:44 +0200

    Implemented Endpoint For completing the registration

---
+++ src/main/java/com/fnb/bheki97/chatappbackendspringboot/controller/registration/RegistrationController.java
@@ -0,0 +1,33 @@
+package com.fnb.bheki97.chatappbackendspringboot.controller.registration;
+
+import com.fnb.bheki97.chatappbackendspringboot.entity.Geek;
+import com.fnb.bheki97.chatappbackendspringboot.service.geekverifier.GeekVerifier;
+import com.fnb.bheki97.chatappbackendspringboot.service.registration.GeekRegistration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registration")
+public class RegistrationController {
+
+    @Autowired
+    private GeekVerifier verifier;
+    @Autowired
+    private GeekRegistration registration;
+
+    @PostMapping
+    public boolean completeRegistration(@RequestBody Geek geek){
+
+        return registration.persistGeekInformation(geek);
+    }
+
+    @PostMapping("/verify")
+    public boolean verifyGeekInfo(@RequestBody Geek geek){
+
+        return verifier.verifyGeek(geek);
+    }
+
+}
