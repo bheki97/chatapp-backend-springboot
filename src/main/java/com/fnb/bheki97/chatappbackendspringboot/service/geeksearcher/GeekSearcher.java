81056d3f1b25fc986f3b555b8c54bab52d838963
bheki97 <bfmokoena7@gmail.com>
2023-12-14 09:36:40 +0200

    changes made

---
+++ src/main/java/com/fnb/bheki97/chatappbackendspringboot/service/geeksearcher/GeekSearcher.java
@@ -0,0 +1,8 @@
+package com.fnb.bheki97.chatappbackendspringboot.service.geeksearcher;
+
+import java.util.List;
+
+public interface GeekSearcher {
+
+    List<String> searchForGeekUsernameByUsername(String username);
+}
